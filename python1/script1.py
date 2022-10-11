4283aa266745e2bdaa49d766a4538178ca998b9c
student <student@info-10.cshl.edu>
2022-10-11 10:50:41 -0400

    Py problemset 1

---
+++ python1/script1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+# this is where I'll put my name
+myName = sys.argv[1]
+# this is where my favorite color is defined
+favColor = sys.argv[2]
+# this is where my favorite activity is defined
+favActivity = sys.argv[3]
+# this is where my favorite animal goes
+favAnimal = sys.argv[4]
+
+#commenting out so I can make a single print argument
+
+#Here is where we will print things out
+#print('My name: ' + myName)
+#This is my favorite color printed out
+#print('My favorite color: ' + favColor)
+#This is my favorite activity printed out
+#print('My favorite activity:' , favActivity)
+#This is my favorite animal
+#print('My favorite animal:' , favAnimal )
+
+print('My name:' + myName + '\n' + 'My color:' + favColor + '\n' + 'My favorite activity:' +  favActivity + '\n' + 'My favorite animal:' + favAnimal) 
