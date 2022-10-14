e04d9e6e547af77fcf23e2af05afd5319d1fa576
Reed A <n.reed.alexander@gmail.com>
2022-10-14 15:26:55 -0400

    last dictionary project

---
+++ pythonday4/lastdic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+fav_dict = {'book': 'spatial ecology', 'song': 'Tom Petty - The Last DJ',' tree' :'sugar maple'}
+
+for key,value in fav_dict.items():
+    print(f'this is the key: {key}, this is the value: {value}')
+
