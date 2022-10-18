ce4ad2bb1dc9c2dfab150e3fb6488cda9e466ac6
Reed A <n.reed.alexander@gmail.com>
2022-10-18 11:40:56 -0400

    counting

---
+++ countingkmers/fastq_file_to_sequence_list.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+## Hot Tip: The needle in the hay stack quit relying on others, and set off to find itself!
+
+## method: seq_list_from_fastq_file(fastq_filename)
+##
+##  Extracts the sequence lines from a fastq file and returns a list
+##  of the sequence lines
+##
+##  input parameters:
+##
+##  fastq_filename :  name of the fastq file (type: string)
+##
+##  returns seq_list : list of read sequences.
+##                    ie.  ["GATCGCATAG", "CGATGCAG", ...]
+    
+def seq_list_from_fastq_file(fastq_filename):
+
+    seq_list = list()
+
+    ## begin your code
+
+    lineofinterest = 2
+    with open(fastq_filename,'r') as fastq:
+        for line in fastq:
+            line = line.strip()
+            lineofinterest += 1
+            if lineofinterest%4 == 0:
+                seq_list.append(line) 
+
+    ## end your code
+
+    return seq_list
+
+
+
+def main():
+
+    progname = sys.argv[0]
+    
+    usage = "\n\n\tusage: {} filename.fastq num_seqs_show\n\n\n".format(progname)
+    
+    if len(sys.argv) < 3:                                       ## we use this to evaluate
+        sys.stderr.write(usage)                                 ## we have the correct num
+        sys.exit(1)                                             ## of input arguments
+
+    # capture command-line arguments
+    fastq_filename = sys.argv[1]
+    num_seqs_show = int(sys.argv[2])
+
+    seq_list = seq_list_from_fastq_file(fastq_filename)         # list that contains seq from 
+                                                                # fastq file
+    print(seq_list[0:num_seqs_show])
+
+    sys.exit(0)  # always good practice to indicate worked ok!
+
+
+
+if __name__ == '__main__':
+    main()
+    
