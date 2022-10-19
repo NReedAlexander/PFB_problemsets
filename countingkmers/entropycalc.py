ba54b4b9db475a82346d9af543dd39c664550c24
Reed A <n.reed.alexander@gmail.com>
2022-10-19 09:08:56 -0400

    your message here

---
+++ countingkmers/entropycalc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+##method: entropy_calc(kmer_dic)
+##
+##  Calculates the Shannon Entropy of each kmer key in a dictionary
+##
+##  input(kmer_dict) 
+##
+
